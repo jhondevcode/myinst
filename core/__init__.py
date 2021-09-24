4d6cb64b130104a17ff8178107f7172295b47ec5
jhondevcode <jhondev.code@gmail.com>
2021-09-24 09:40:08 -0500

    The main skeleton for the execution code was established

--- core/__init__.py
+++ core/__init__.py
@@ -4,3 +4,45 @@
 The core package has the necessary modules to: operate files, detect the system,
 verify updates, establish configurations and many more utilities.
 """
+
+from core.utilities import clear
+from core.color import print_bwhite
+from core.color.levels import error, info, success
+from core.color.colors import yellow
+from config import execute_config_module
+from lang import execute_lang_module
+from tool import execute_tools_module
+
+
+def welcome_screen():
+    pass
+
+
+def show_options():
+    while True:
+        print_bwhite("=> Enter an option <=")
+        print_bwhite("1) Tools")
+        print_bwhite("2) Languages")
+        print_bwhite("3) Configurations")
+        print_bwhite("4) Exit")
+        option = input(yellow("Option [1-4]: "))
+        if option == "1":
+            execute_tools_module()
+        elif option == "2":
+            execute_lang_module()
+        elif option == "3":
+            execute_config_module()
+        elif option == "4":
+            clear()
+            info("Please visit https://github.com/jhondevcode")
+            success("Thanks for using the myinst script ;)")
+            break
+        else:
+            clear()
+            error("Requested option not found")
+
+
+def start():
+    clear()
+    welcome_screen()
+    show_options()
