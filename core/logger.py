e5c2b98f6cd4ef1bee9c83c6164f9c9824eebd91
jhondevcode <jhondev.code@gmail.com>
2021-09-23 19:00:54 -0500

    Added a preconfigured logging module

---
+++ core/logger.py
@@ -0,0 +1,25 @@
+import datetime
+import logging
+import os
+
+LOG_DIR = os.path.join(str(__file__).replace("/core/logger.py", ""), "logs")
+
+if not os.path.isdir(LOG_DIR):
+    os.mkdir(LOG_DIR)
+
+log_file = os.path.join(LOG_DIR, f"log-{datetime.datetime.now().date()}.log")
+logging.basicConfig(filename=log_file, format="%(asctime)s %(message)s", filemode="a")
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+def info(msg: str):
+    logger.info(msg)
+
+
+def warning(msg: str):
+    logger.warning(msg)
+
+
+def error(msg: str):
+    logger.error(msg)
