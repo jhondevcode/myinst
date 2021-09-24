8d7c3069d302eeb6ef3961d0629cae1ad9ec26d3
jhondevcode <jhondev.code@gmail.com>
2021-09-24 09:21:53 -0500

    Colored bindings for print function

--- core/color/__init__.py
+++ core/color/__init__.py
@@ -4,4 +4,44 @@ It is a binding for the colorama library.
 """
 
 from core.color.levels import *
-import core.color.colors
+from core.color import colors
+
+
+def print_blue(message: str):
+    print(colors.bblue(message))
+
+
+def print_cyan(message: str):
+    print(colors.cyan(message))
+
+
+def print_green(message: str):
+    print(colors.green(message))
+
+
+def print_magenta(message: str):
+    print(colors.magenta(message))
+
+
+def print_red(message: str):
+    print(colors.red(message))
+
+
+def print_yellow(message: str):
+    print(colors.yellow(message))
+
+
+def print_white(message: str):
+    print(colors.white(message))
+
+
+def print_bblue(message: str):
+    print(colors.bblue(message))
+
+
+def print_bmagenta(message: str):
+    print(colors.bmagenta(message))
+
+
+def print_bwhite(message: str):
+    print(colors.bwhite(message))
