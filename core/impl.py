6fa59f67a37dc2cb66f3aa6056d0dc4267648225
jhondevcode <jhondev.code@gmail.com>
2021-09-29 19:13:56 -0500

    Template classes

---
+++ core/impl.py
@@ -0,0 +1,11 @@
+"""
+This module contains unimplemented abstract classes, the classes present here
+are used for multiple purposes.
+"""
+
+
+class InstallWizard:
+    """Template for creating classes for install wizard"""
+
+    def __init__(self):
+        super(InstallWizard, self).__init__()
