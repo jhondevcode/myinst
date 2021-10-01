ee2d2a757f7dcb6140d036e1461eec7257fd0276
jhondevcode <jhondev.code@gmail.com>
2021-09-30 22:50:09 -0500

    Import code has been simplified

--- lang/__init__.py
+++ lang/__init__.py
@@ -5,39 +5,21 @@ configure different programming languages.
 
 from core.utilities import clear, get_os_name, ARCH, MANJARO, DEBIAN, FEDORA
 from core.color import error
-from core.logger import logger
+from lang.debian import DebianLangInstaller
+from lang.arch import ArchLangInstaller, ManjaroLangInstaller
+from lang.fedora import FedoraLangInstaller
 
 
 def execute_lang_module():
     clear()
     os_name = get_os_name()
     if os_name == DEBIAN:
-        try:
-            from lang.debian import DebianLangInstaller
-            installer = DebianLangInstaller()
-            installer.run()
-        except ImportError as ex:
-            logger.error(ex)
+        DebianLangInstaller().run()
     elif os_name == ARCH:
-        try:
-            from lang.arch import ArchLangInstaller
-            installer = ArchLangInstaller()
-            installer.run()
-        except ImportError as ex:
-            logger.error(ex)
+        ArchLangInstaller().run()
     elif os_name == MANJARO:
-        try:
-            from lang.arch import ManjaroLangInstaller
-            installer = ManjaroLangInstaller()
-            installer.run()
-        except ImportError as ex:
-            logger.error(ex)
+        ManjaroLangInstaller().run()
     elif os_name == FEDORA:
-        try:
-            from lang.fedora import FedoraLangInstaller
-            installer = FedoraLangInstaller()
-            installer.run()
-        except ImportError as ex:
-            logger.error(ex)
+        FedoraLangInstaller().run()
     else:
         error("Your operating system is not supported")
